01497ff88a7d632092a3e5c41b3d475ff1672635
Mislav <mislavva@gmail.com>
2023-03-07 19:53:06 +0100

    feat: add datetimepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
+import { Subject, takeUntil } from "rxjs";
+import * as moment from "moment-timezone";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'datepicker';
+export class AppComponent implements OnInit, OnDestroy {
+
+  private readonly _destroy = new Subject<void>();
+
+  formGroup = new FormGroup({
+    datetime: new FormControl(moment()),
+    timezone: new FormControl()
+  });
+
+
+  ngOnInit() {
+    this.formGroup.controls.timezone.valueChanges.pipe(
+      takeUntil(this._destroy)
+    ).subscribe(timezone => {
+      this.formGroup.controls.datetime.setValue(moment.tz(this.formGroup.controls.datetime.value, timezone));
+      console.log(timezone)
+      console.log(this.formGroup.controls.datetime.value)
+    });
+  }
+
+  ngOnDestroy() {
+    this._destroy.next();
+    this._destroy.complete();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatCardModule } from "@angular/material/card";
+import { MtxDatetimepickerModule } from "@ng-matero/extensions/datetimepicker";
+import { MtxMomentDatetimeModule } from "@ng-matero/extensions-moment-adapter";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    ReactiveFormsModule,
+
+    MtxDatetimepickerModule,
+    MtxMomentDatetimeModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
